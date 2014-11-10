a2aa9d2339477e788d71aba4ae9c8800aa148b65
Stéphane HULARD <s.hulard@chstudio.fr>
2014-11-10 11:22:21 +0100

    Test duplication
    Signed-off-by: Stéphane HULARD <s.hulard@chstudio.fr>

--- test/units/EvenementEventEmitterAdapterTest.php
+++ test/units/EvenementEventEmitterAdapterTest.php
@@ -17,42 +17,14 @@ use Bee4\Events\Adapters\EvenementEventEmitterAdapter;
  * Check behaviour Événement EventEmitter adapter
  * @package Bee4\Test\Events
  */
-class EvenementEventEmitterAdapterTest extends \PHPUnit_Framework_TestCase {
-	/**
-	 * @var \Evenement\EventEmitter
-	 */
-	protected $dispatcher;
-
+class EvenementEventEmitterAdapterTest extends AbstractAdapterTest {
 	protected function setUp() {
 		if( !class_exists("Evenement\EventEmitter") ) {
 			$this->markTestSkipped('You must install dev dependencies to be able run tests!');
 		}
 
-		$this->dispatcher = new \Evenement\EventEmitter();
-	}
-
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage Yes event triggered
-	 */
-	public function testAdapter() {
-		$adapter = new EvenementEventEmitterAdapter($this->dispatcher);
-		$handler = function() { echo "Youpi!"; };
-		$event = $this->getMock('Bee4\Events\EventInterface');
-
-		$this->expectOutputString('Youpi!');
-
-		$adapter->add('name', $handler);
-		$adapter->dispatch('name', $event);
-
-		$adapter->add('name', function() {
-			throw new \Exception('Yes event triggered');
-		});
-
-		$this->assertCount(2, $adapter->get('name'));
-		$adapter->remove('name', $handler);
-		$this->assertCount(1, $adapter->get('name'));
-
-		$adapter->dispatch('name', $event);
+		$this->adapter = new EvenementEventEmitterAdapter(
+			new \Evenement\EventEmitter()
+		);
 	}
 }
--- test/units/SymfonyEventDispatcherAdapterTest.php
+++ test/units/SymfonyEventDispatcherAdapterTest.php
@@ -17,42 +17,14 @@ use Bee4\Events\Adapters\SymfonyEventDispatcherAdapter;
  * Check behaviour SF2 EventDispatcher adapter
  * @package Bee4\Test\Events
  */
-class SymfonyEventDispatcherAdapterTest extends \PHPUnit_Framework_TestCase {
-	/**
-	 * @var \Symfony\Component\EventDispatcher\EventDispatcher
-	 */
-	protected $dispatcher;
-
+class SymfonyEventDispatcherAdapterTest extends AbstractAdapterTest {
 	protected function setUp() {
 		if( !class_exists("Symfony\Component\EventDispatcher\EventDispatcher") ) {
 			$this->markTestSkipped('You must install dev dependencies to be able run tests!');
 		}
 
-		$this->dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-	}
-
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage Yes event triggered
-	 */
-	public function testAdapter() {
-		$adapter = new SymfonyEventDispatcherAdapter($this->dispatcher);
-		$handler = function() { echo "Youpi!"; };
-		$event = $this->getMock('Bee4\Events\EventInterface');
-
-		$this->expectOutputString('Youpi!');
-
-		$adapter->add('name', $handler);
-		$adapter->dispatch('name', $event);
-
-		$adapter->add('name', function() {
-			throw new \Exception('Yes event triggered');
-		});
-
-		$this->assertCount(2, $adapter->get('name'));
-		$adapter->remove('name', $handler);
-		$this->assertCount(1, $adapter->get('name'));
-
-		$adapter->dispatch('name', $event);
+		$this->adapter = new SymfonyEventDispatcherAdapter(
+			new \Symfony\Component\EventDispatcher\EventDispatcher()
+		);
 	}
 }
